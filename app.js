2a9553b234b90db42265a5dca2abc66410bdb297
personal <codewithlinds@gmail.com>
2024-05-13 21:57:36 -0500

    updated package.json with compression package

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const express = require("express");
 const morgan = require("morgan");
+const compression = require("compression");
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
 const mongoSanitize = require("express-mongo-sanitize");
@@ -71,9 +72,12 @@ app.use(
   })
 );
 
+app.use(compression());
+
 // Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
+  // console.log(req.cookies);
   next();
 });
 
